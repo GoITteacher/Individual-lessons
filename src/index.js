b8893ea88555ffdeec85bee6044215be17817743
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-06-08 08:55:35 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const array = ["HTML", "CSS", "REACT", "REDUX", "JavaScript", "NodeJs"];
+const array2 = [
+  {
+    title: "HTML",
+    developers: "2000",
+  },
+  {
+    title: "CSS",
+    developers: "2000",
+  },
+  {
+    title: "REACT",
+    developers: "2000",
+  },
+  {
+    title: "REDUX",
+    developers: "2000",
+  },
+  {
+    title: "JavaScript",
+    developers: "2000",
+  },
+  {
+    title: "NodeJs",
+    developers: "2000",
+  },
+];
