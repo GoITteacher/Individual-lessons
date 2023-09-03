e8dec5767ce43aaf0735e9fcf8f6dc6c6a612d0c
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-03 14:05:06 +0300

    123

--- src/Bank.js
+++ src/Bank.js
@@ -1,1 +1,15 @@
+class Bank {
+  balance = 0;
+  transactions = [];
 
+  deposit(amount) {}
+  withdraw(amount) {}
+  createTransaction(amount, type) {
+    const transaction = {
+      id: this.transactions.length,
+      type,
+      amount,
+    };
+    this.transactions.push(transaction);
+  }
+}
