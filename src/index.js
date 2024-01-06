c56677cf1c29618c950ed0d9eba790386c84a24a
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-06 17:09:52 +0200

    update

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const TelegramBot = require("node-telegram-bot-api");
-
-const bot = new TelegramBot(token, { polling: true });
-bot.on("message", (msg) => {
-  console.log(msg);
-});
-
-bot.on("video", () => {});
