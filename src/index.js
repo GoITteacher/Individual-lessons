7312c9c88209614c4bf95d9d94c0276a2fa0d403
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-07 16:54:24 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const arr = [6, 1, 4, 8];
+
+// const result = arr.reduce((acc, elem, idx, arr) => {
+//   if (elem % 2 === 0) {
+//     return acc + elem;
+//   } else {
+//     return acc;
+//   }
+// });
+
+// const result = arr.reduce((acc, elem, idx, arr) => {
+//   if (elem % 2 === 0) {
+//     acc.push(elem);
+//   }
+
+//   return acc;
+// }, []);
+
+// console.log(result);
+
+// 1 5 6 8
